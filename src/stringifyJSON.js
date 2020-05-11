fc1ec8bb9a0be70a13be4dbcb7db779efce6c4a2
Erfanch <erfan.chinif@gmail.com>
2020-05-11 16:00:28 -0700

    write conditional statement for number,null and boolean

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,28 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  // var result = '';
+// if input is a number:
+  if (typeof obj === 'number') {
+    return '' + obj;
+  }
+
+  // if input is undefined:
+  // if ( obj === undefined) {
+  //   return null
+  // }
+  // if input is Null
+    if (obj === null) {
+      return 'null'
+  }
+// if input is boolean
+  if (typeof obj === 'boolean') {
+    return '' + obj
+  }
+// if input is string
+// if input is an array
+// if the input is an obj
+
   // your code goes here
+  // return result;
 };
